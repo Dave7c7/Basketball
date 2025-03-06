94f0e3301bb7c98047577496b486fe1d44d0f94d
Dave7c7 <dave7c7@gmail.com>
2025-03-06 15:22:17 -0500

    main branch

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let count = document.getElementById('Counter');
-let result = 0;
-function addOne() {
-    result++;
-    count.innerHTML = result;
+let GuessCounter = document.getElementById('GuessCounter');
+let HomeCounter = document.getElementById('HomeCounter');
+//single function
+let guestcount = 0;
+let homecount = 0;
+function increment(player, amount) {
+    if(player === 'forGuess') {
+        guestcount += amount;
+        GuessCounter.textContent = guestcount;
+    }
+    else if(player === 'forHome') {
+        homecount += amount;
+        HomeCounter.textContent = homecount;
+    }
 }
