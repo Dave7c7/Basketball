a4837d3fd2a5c347113f0bff46d1cb57eec542b3
Dave7c7 <dave7c7@gmail.com>
2025-03-07 18:17:46 -0500

    main branch

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let HomeCounter = document.getElementById('HomeCounter');
 //single function
 let guestcount = 0;
 let homecount = 0;
+    GuessCounter.classList.remove('highlight');
+    HomeCounter.classList.remove('highlight'); 
 function increment(player, amount) {
     if(player === 'forGuess') {
         guestcount += amount;
@@ -12,4 +14,23 @@ function increment(player, amount) {
         homecount += amount;
         HomeCounter.textContent = homecount;
     }
+    cheackLeader();
+}
+
+function cheackLeader(){
+    if(guestcount > homecount){
+        GuessCounter.classList.add('highlight');
+        HomeCounter.classList.remove('highlight'); 
+    }
+    else if(homecount > guestcount){
+        HomeCounter.classList.add('highlight');
+        GuessCounter.classList.remove('highlight');
+    }
+}
+function newGame(){
+    guestcount, homecount = 0;
+    GuessCounter.textContent = 0;
+    HomeCounter.textContent = 0;
+    GuessCounter.classList.remove('highlight');
+    HomeCounter.classList.remove('highlight');
 }
